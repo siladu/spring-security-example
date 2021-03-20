891c863647ee401ab52aa8121d4bfab706759282
simon.dudley <simon.dudley@autotrader.co.uk>
2021-03-20 11:17:32 +0000

    Spring Initizr spring web and security

---
+++ src/test/java/com/simondudley/example/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simondudley.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
